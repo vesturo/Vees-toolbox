91f877fac6ccdde4f12bb4e2a139fffb9599d275
Vesturo <37129099+vesturo@users.noreply.github.com>
2024-04-11 20:04:10 +0200

    added PostProcessing handler for VideoTXL and ProTV

---
+++ scripts/PostProcessing Switch/ProTV/script/ProTVPostProcessingHandler.cs
@@ -0,0 +1,67 @@
+﻿
+using ArchiTech.ProTV;
+using Texel;
+using UdonSharp;
+using UnityEngine;
+using UnityEngine.Playables;
+using UnityEngine.Video;
+using VRC.SDKBase;
+using VRC.Udon;
+
+namespace ArchiTech.ProTV
+{
+    public enum CustomPlayerState { STOPPED, PAUSED, ERROR, PLAYING }
+    public class ProTVPostProcessingHandler : TVPlugin
+    {
+        [HideInInspector] public CustomPlayerState playerState;
+        public Animator PostProcessing;
+        public override void Start()
+        {
+            if (init) return; // prevent start from being called accidentally more than once.
+            base.Start(); // sets up all the TV stuff.
+        }
+
+        public override void _TvStop()
+        {
+            playerState = CustomPlayerState.STOPPED;
+            SetState(playerState);
+        }
+        public override void _TvPlay()
+        {
+            playerState = CustomPlayerState.PLAYING;
+            SetState(playerState);
+        }
+        public override void _TvPause()
+        {
+            playerState = CustomPlayerState.PAUSED;
+            SetState(playerState);
+        }
+        public override void _TvVideoPlayerError()
+        {
+            playerState = CustomPlayerState.ERROR;
+            SetState(playerState);
+        }
+        public void SetState(CustomPlayerState playerState)
+        {
+            switch (playerState)
+            {
+                case CustomPlayerState.STOPPED:
+                    if (IsDebugEnabled) Debug($"Current Playerstate is {playerState}");
+                    PostProcessing.SetBool("VideoPlaying", false);
+                    break;
+                case CustomPlayerState.PAUSED:
+                    if (IsDebugEnabled) Debug($"Current Playerstate is {playerState}");
+                    PostProcessing.SetBool("VideoPlaying", false);
+                    break;
+                case CustomPlayerState.ERROR:
+                    if (IsDebugEnabled) Debug($"Current Playerstate is {playerState}");
+                    PostProcessing.SetBool("VideoPlaying", false);
+                    break;
+                case CustomPlayerState.PLAYING:
+                    if (IsDebugEnabled) Debug($"Current Playerstate is {playerState}");
+                    PostProcessing.SetBool("VideoPlaying", true);
+                    break;
+            }
+        }
+    }
+}
---
+++ scripts/PostProcessing Switch/VideoTXL/script/VIdeoTXLPostProcessingHandler.cs
@@ -0,0 +1,37 @@
+﻿
+using Texel;
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+[UdonBehaviourSyncMode(BehaviourSyncMode.None)]
+public class VIdeoTXLPostProcessingHandler : UdonSharpBehaviour
+{
+    public TXLVideoPlayer videoPlayer;
+    public Animator PostProcessing;
+
+    void Start()
+    {
+        videoPlayer._Register(TXLVideoPlayer.EVENT_VIDEO_STATE_UPDATE, this, nameof(_OnVideoStateUpdate));
+    }
+
+    public void _OnVideoStateUpdate()
+    {
+        switch (videoPlayer.playerState)
+        {
+            case TXLVideoPlayer.VIDEO_STATE_STOPPED:
+                PostProcessing.SetBool("VideoPlaying", false);
+                break;
+            case TXLVideoPlayer.VIDEO_STATE_ERROR:
+                PostProcessing.SetBool("VideoPlaying", false);
+                break;
+            case TXLVideoPlayer.VIDEO_STATE_LOADING:
+                PostProcessing.SetBool("VideoPlaying", false);
+                break;
+            case TXLVideoPlayer.VIDEO_STATE_PLAYING:
+                PostProcessing.SetBool("VideoPlaying", true);
+                break;
+        }
+    }
+}
