4030504c408bc22b3294d401dd658aea3a2d177c
Vesturo <37129099+vesturo@users.noreply.github.com>
2024-07-31 15:08:14 +0200

    added GI updater handler

---
+++ scripts/VRSL/VRSLGi/Gi_updater_handler.cs
@@ -0,0 +1,24 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+
+namespace VRSL
+{
+    [UdonBehaviourSyncMode(BehaviourSyncMode.None)]
+    public class Gi_updater_handler : UdonSharpBehaviour
+    {
+        public VRSL_GI_Function VRSL_GI_Function;
+        [Range(1.0f, 30.0f)] public float Timeout;
+
+        public void _UpdatePosition()
+        {
+            VRSL_GI_Function.isRunning = true;
+            SendCustomEventDelayedSeconds("_ResetFunction", Timeout);
+        }
+
+        public void _ResetFunction()
+        {
+            VRSL_GI_Function.isRunning = false;
+        }
+    }
+}
